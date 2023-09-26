4cf826c13aec84d67b1f73641594663190a02dee
VitorHRV <62753727+VitorXt@users.noreply.github.com>
2023-09-26 19:57:47 -0300

    Add files via upload

---
+++ Prova1/Controllers/CartaController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Prova1.Models.Request;
+
+namespace Prova1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CartaController : ControllerBase
+    {
+
+        private readonly string _cartacaminhoArquivo;
+
+        public CartaController()
+        {
+            _cartacaminhoArquivo = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Carta.json");
+        }
+
+        #region Operaçoes arquivo
+
+        private List<RegistraCartaViewModel> LerCartaDoArquivo()
+        {
+            if (!System.IO.File.Exists(_cartacaminhoArquivo))
+            {
+                return new List<RegistraCartaViewModel>();
+            }
+
+            string json = System.IO.File.ReadAllText(_cartacaminhoArquivo);
+            return JsonConvert.DeserializeObject<List<RegistraCartaViewModel>>(json);
+        }
+
+        private void EscreverCartasNoArquivo(List<RegistraCartaViewModel> cartas)
+        {
+            string json = JsonConvert.SerializeObject(cartas);
+            System.IO.File.WriteAllText(_cartacaminhoArquivo, json);
+        }
+
+        #endregion
+
+        #region POST
+
+        [HttpPost]
+        public IActionResult Post(RegistraCartaViewModel registraCartaViewModel)
+        {
+
+            var CartasRealizadas = LerCartaDoArquivo();
+
+            CartasRealizadas.Add(registraCartaViewModel);
+
+            EscreverCartasNoArquivo(CartasRealizadas);
+
+            return Ok("Carta registrada com sucesso");
+        }
+
+        #endregion
+
+        #region GET 
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(LerCartaDoArquivo());
+        }
+
+        #endregion
+    }
+}
---
+++ Prova1/Models/Request/RegistraCartaViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Prova1.Models.Request
+{
+    public class RegistraCartaViewModel
+    {
+        [Required(ErrorMessage = "O Nome é obrigatório.")]
+        [StringLength(255, MinimumLength = 3, ErrorMessage = "O Nome deve ter no mínimo 3 caracteres e no máximo 255 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A Rua é obrigatória.")]
+        public string Rua { get; set; }
+
+        [Required(ErrorMessage = "O Numero do endereço é obrigatório.")]
+        public int Numero { get; set; }
+
+        [Required(ErrorMessage = "O Bairro é obrigatório.")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "A Cidade é obrigatória.")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "A Idade é obrigatório.")]
+        [Range(1, 15, ErrorMessage = "Não é permitido maiores de 15 anos")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "A Carta é obrigatória.")]
+        [MaxLength(500, ErrorMessage = "A Carte tem que ter no máximo 500 caractéres.")]
+        public string Carta { get; set; }
+    }
+}
